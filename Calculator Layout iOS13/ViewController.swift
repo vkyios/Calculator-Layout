b13493ef7ca2fc5c2848f25e9391fe79b5dccab3
Wahyd <wahyd@Wahyds-Mac-mini.local>
2020-12-22 15:42:12 +0500

    First commit

---
+++ Calculator Layout iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator Layout iOS13
+//
+//  Created by vky on 01/07/2019.
+//  Copyright © 2019 vky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
